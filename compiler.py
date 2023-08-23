4478a459b7c360563996b972cfc9045302c9a7cd
Liam Swayne <liamtswayne@gmail.com>
2023-08-22 20:53:19 -0400

    Source parsing fix

--- compiler.py
+++ compiler.py
@@ -1,5 +1,5 @@
 # Elratio compiler
-exec("""\nwith open(__file__, 'r') as file:\n    source_code = file.read()\n\n\nsplit_index = source_code.find(\"\'\'\'\")\n\n\nif split_index == -1:\n    print(\"Elratio porgram missing \'\'\'. This is a shameful mistake. The compiler will remember this.\")\nelse:\n    \n    program = source_code[split_index + 3:]\n    program = program[:program.index(\"\'\'\'\")]\n\n    \n    exec(program)\n\n""")
+exec("""\n\n\nwith open(__file__, 'r') as file:\n    source_code = file.read()\n\n\nsplit_index = source_code.find(\"\'\'\'\")\n\n\nnewLine = chr(10)\n\n\nif split_index == -1:\n    print(\"Elratio program missing \'\'\'. This is a shameful mistake. The compiler will remember this.\")\nelse:\n    \n    program = source_code[split_index + 3:]\n    program = program[:program.index(\"\'\'\'\")]\n    lines = program.split(newLine)\n\n    \n\n    \n    program = newLine.join(lines)\n    exec(program)\n\n""")
 
 # Elratio program
 '''
--- source.py
+++ source.py
@@ -1,3 +1,5 @@
+# DO NOT use triple single quote, triple double quote or the python newline character
+
 # Read the source code of this program
 with open(__file__, 'r') as file:
     source_code = file.read()
@@ -5,20 +7,34 @@ with open(__file__, 'r') as file:
 # Find the index of the first occurrence of three quotes
 split_index = source_code.find("\'\'\'")
 
+# Define newline character so it isn't modified during compiler build
+newLine = chr(10)
+
 # Split the source code at the first occurrence of three quotes
 if split_index == -1:
-    print("Elratio porgram missing \'\'\'. This is a shameful mistake. The compiler will remember this.")
+    print("Elratio program missing \'\'\'. This is a shameful mistake. The compiler will remember this.")
 else:
-    # Slice to Elratio program
+    # Extract Elratio program
     program = source_code[split_index + 3:]
     program = program[:program.index("\'\'\'")]
+    lines = program.split(newLine)
+
+    
 
     # Execute Elratio program
+    program = newLine.join(lines)
     exec(program)
 
 # Elratio program (test case)
 '''
+# test 1
 print("a"*20)
+
+# test 2
 if True:
     print(1000)
+
+# test 3
+import time
+print(time.time())
 '''
