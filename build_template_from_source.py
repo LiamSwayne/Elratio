84d6efacf6ff474ae7f8eb39ea9086bd2e1af37c
Liam Swayne <liamtswayne@gmail.com>
2023-09-19 20:01:44 -0400

    updated second level version

--- build_template_from_source.py
+++ build_template_from_source.py
@@ -46,7 +46,7 @@ while cleanedCode[0] == "\\" and cleanedCode[1] == "n":
 while cleanedCode[-2] == "\\" and cleanedCode[-1] == "n":
     cleanedCode = cleanedCode[:-2]
 days = ['monday','tuesday','wednesday','thursday','friday','saturday','sunday']
-wrappedCode = "# Elratio compiler version "+str(abs(hash(cleanedCode)) % 10000000)+'.'+str(abs(hash(str(len(cleanedCode)))))+"."+days[abs(hash(cleanedCode)) % 7]+"\nexec(\"\"\"" + cleanedCode + "\"\"\")\n\n# Elratio program (all code must go inside the quotes)\n\'\'\'\n\n\'\'\'"
+wrappedCode = "# Elratio compiler version "+str(abs(hash(cleanedCode)) % 10000000)+'.'+str(abs(hash(str(len(cleanedCode)))) % 10000000)+"."+days[abs(hash(cleanedCode)) % 7]+"\nexec(\"\"\"" + cleanedCode + "\"\"\")\n\n# Elratio program (all code must go inside the quotes)\n\'\'\'\n\n\'\'\'"
 
 compilerFile = open('template.py', 'w')
 compilerFile.write(wrappedCode)
